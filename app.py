c35a1030a1aef1c0b431841000884b80f5da3a4b
Nisha George <negeorge@gmail.com>
2012-06-03 20:47:26 -0700

    changed to game day logic

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
+import os
+import json
+import datetime
+
 from flask import Flask, request, redirect
 import twilio.twiml
-import os
+
 app = Flask(__name__)
- 
+app.debug = True
+
+schedule = json.load(open('giants2012schedule.json'))
 @app.route("/")
-def hello_monkey():
-    """Respond to incoming calls with a simple text message."""
- 
+def game_schedule():
+    """Respond to incoming sms with if there is a game."""
     resp = twilio.twiml.Response()
-    resp.sms("Hi From Nisha's app")
+    todays_date = datetime.datetime.now().strftime("X%m/X%d/%Y")
+    todays_date = todays_date.replace('X0','X').replace('X', '')
+    
+    for game in schedule['games']:
+        if todays_date == game['date']:
+            resp.sms("Giants Game Today! %s %s %s" % (game['time'], game['opponent'], game['location']))
+            return str(resp)
+			
+    resp.sms("No Giants Game Today")
     return str(resp)
  
 if __name__ == "__main__":
