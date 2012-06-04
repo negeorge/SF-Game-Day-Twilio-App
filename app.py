b4eb4e5e3d6b9ddfaeae06fd3a2740e3f5b4d453
Nisha George <negeorge@gmail.com>
2012-06-03 19:33:25 -0700

    first twilio commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, redirect
+import twilio.twiml
+import os
+app = Flask(__name__)
+ 
+@app.route("/")
+def hello_monkey():
+    """Respond to incoming calls with a simple text message."""
+ 
+    resp = twilio.twiml.Response()
+    resp.sms("Hello, Mobile Monkey")
+    return str(resp)
+ 
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
